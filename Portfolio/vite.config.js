78dcb9be47ff3b1669911f5b36c671c3b15a7677
Developer_Sameer <105971029+sam12-4@users.noreply.github.com>
2024-04-08 08:49:34 +0500

    Second Commit

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Portfolio_Sameer_Hussain",
   plugins: [react()],
 assetsInclude : ['**/*.glb']
 })
